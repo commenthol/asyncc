cd51feb86505911c001861782f56d2b5eba4ca75
commenthol <commenthol@gmail.com>
2025-02-27 20:00:40 +0100

    fix: commonjs exports

--- scripts/rollup.conf.mjs
+++ scripts/rollup.conf.mjs
@@ -1,41 +1,37 @@
 // import babel from 'rollup-plugin-babel'
-import terser from '@rollup/plugin-terser'
+import terser from "@rollup/plugin-terser";
 
-process.env.BABEL_ENV = 'es5'
+process.env.BABEL_ENV = "es5";
 
 export default [
   {
-    input: 'src/index.js',
-    output: [{
-      file: './dist/index.es.js',
-      format: 'es'
-    }, {
-      file: './dist/index.js',
-      format: 'cjs',
-      exports: 'named'
-    }],
-    plugins: [
-    ]
+    input: "src/index.js",
+    output: [
+      {
+        file: "./dist/index.es.js",
+        format: "es",
+      },
+      {
+        file: "./dist/index.cjs",
+        format: "cjs",
+        exports: "named",
+      },
+    ],
+    plugins: [],
   },
   {
-    input: 'src/index.js',
-    output: [{
-      file: './dist/index.min.js',
-      format: 'cjs',
-      exports: 'named'
-    }],
-    plugins: [
-      terser()
-    ]
+    input: "src/index.js",
+    output: [
+      {
+        file: "./dist/index.min.cjs",
+        format: "cjs",
+        exports: "named",
+      },
+      {
+        file: "./dist/index.es.min.js",
+        format: "es",
+      },
+    ],
+    plugins: [terser()],
   },
-  {
-    input: 'src/index.js',
-    output: [{
-      file: './dist/index.es.min.js',
-      format: 'es'
-    }],
-    plugins: [
-      terser()
-    ]
-  }
-]
+];
